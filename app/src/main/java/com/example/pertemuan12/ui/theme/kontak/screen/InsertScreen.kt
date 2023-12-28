ad412c89fb5fff4f47aba5b32ada15877c0ea8b5
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-12-28 10:57:45 +0700

    membuatt EntryKontakBody

--- app/src/main/java/com/example/pertemuan12/ui/theme/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/kontak/screen/InsertScreen.kt
@@ -1,25 +1,58 @@
 package com.example.pertemuan12.ui.theme.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.KontakAPP.TopAppBarKontak
 import com.example.pertemuan12.navigation.DestinasiNavigasi
 import com.example.pertemuan12.ui.theme.PenyediaViewModel
+import com.example.pertemuan12.ui.theme.kontak.viewmodel.InsertUIEvent
+import com.example.pertemuan12.ui.theme.kontak.viewmodel.InsertUIState
 import com.example.pertemuan12.ui.theme.kontak.viewmodel.InsertViewModel
 import kotlinx.coroutines.launch
 
 
 
+@Composable
+fun EntryKontakBody(
+    insertUiState:InsertUIState,
+    onSiswaValueChange: (InsertUIEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInputSiswa(
+            insertUIEvent = insertUiState.insertUIEvent,
+            onValueChange = onSiswaValueChange,
+            modifier=Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = "Simpan")
+        }
+    }
+}
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item entry"
@@ -52,9 +85,9 @@ fun EntryKontakScreen(
                 }
             },
             modifier = Modifier
-                    .padding(innerPadding)
-                    .verticalScroll (rememberScrollState())
-                    .fillMaxWidth()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
 
     }
 }
