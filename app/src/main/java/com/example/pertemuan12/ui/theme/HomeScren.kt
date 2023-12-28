9271a386d56c1c225d759ca28e3dd24e2780b417
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-12-28 09:56:03 +0700

    membuat function homescreen

--- app/src/main/java/com/example/pertemuan12/ui/theme/HomeScren.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/HomeScren.kt
@@ -1,10 +1,18 @@
 package com.example.pertemuan12.ui.theme
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 import com.example.pertemuan12.navigation.DestinasiNavigasi
 
 object DestinasiHome:DestinasiNavigasi{
     override val route="home"
     override val titleRes="Kontak"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+
+){}
 
 
