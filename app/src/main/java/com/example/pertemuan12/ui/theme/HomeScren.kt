13e6257e69f040de3d9d7068c282fd62f74e1349
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-12-28 10:08:27 +0700

    menyelesaikan function homescreen

--- app/src/main/java/com/example/pertemuan12/ui/theme/HomeScren.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/HomeScren.kt
@@ -1,8 +1,26 @@
 package com.example.pertemuan12.ui.theme
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MonotonicFrameClock
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.KontakAPP.TopAppBarKontak
 import com.example.pertemuan12.navigation.DestinasiNavigasi
+import com.example.pertemuan12.ui.theme.home.viewmodel.HomeViewModel
+import java.util.jar.JarEntry
 
 object DestinasiHome:DestinasiNavigasi{
     override val route="home"
@@ -12,7 +30,43 @@ object DestinasiHome:DestinasiNavigasi{
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier:Modifier=Modifier,
+    onDetailClick:(Int)->Unit={},
+    viewModel:HomeViewModel=viewModel(factory = PenyediaViewModel.Factory)
 
-){}
+){
+    val scrollBehavior=TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier =modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title =DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior=scrollBehavior)
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = navigateToItemEntry,
+                shape= MaterialTheme.shapes.medium,
+                modifier=Modifier.padding(18.dp)) {
+               Icon(
+                   imageVector  = Icons.Default.Add,
+                   contentDesciption = "Add Kontak"
+               )
+            }
+        },
+    ){
+        innerPadding ->
+        HomeStatus(
+            kontakUIState = viewModel.kontakUIState,
+            retryAction = { viewModel.getKontak()},
+            modifier = Modifier.padding(innerPadding)),
+            onDetailClick=onDetailClick,
+            onDeleteClick={
+                viewModel.deleteKontak(it.id)
+                viewModel.getKontak()
+            }
+    }
+}
 
 
