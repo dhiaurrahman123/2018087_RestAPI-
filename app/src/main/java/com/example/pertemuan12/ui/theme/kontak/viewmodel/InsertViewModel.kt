2a7454f6ba1a9e386ef6d7a09cee37f26c34ad61
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-12-28 10:20:29 +0700

    menyelesaikan  function updateInsertKontakState

--- app/src/main/java/com/example/pertemuan12/ui/theme/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/kontak/viewmodel/InsertViewModel.kt
@@ -3,7 +3,9 @@ package com.example.pertemuan12.ui.theme.kontak.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val kontakRepository: KontakRepository): ViewModel(){
     var insertKontakState by mutableStateOf(InsertUiState())
@@ -11,5 +13,13 @@ class InsertViewModel (private val kontakRepository: KontakRepository): ViewMode
     fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
         insertKontakState= InsertUIState(insertUiEvent = insertUiEvent)
     }
-
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertkontak(insertKontakState.insertUIEvent.toKontak())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
