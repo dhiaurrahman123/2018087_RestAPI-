ed60457c5e39c5531f9f9ce2586b2bb386952f05
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-12-28 09:20:53 +0700

    menyelesaikan function TopAppBarKontak

--- app/src/main/java/com/example/pertemuan12/KontakAPP/KontakApp.kt
+++ app/src/main/java/com/example/pertemuan12/KontakAPP/KontakApp.kt
@@ -2,6 +2,8 @@ package com.example.pertemuan12.KontakAPP
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -44,8 +46,27 @@ fun KontakApp(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppBarKontak(
+    title:String,
+    canNavigateBack:Boolean,
+    modifier:Modifier=Modifier,
+    scrollBehavior:TopAppBarScrollBehavior?=null,
+    navigateUp:()->Unit={}
+){
+    CenterAlignedTopAppBar(title={Text (title)}),
+    modifier = modifier,
+    scrollBehavior = scrollBehavior,
+    navigationIcon= {
+        if (canNavigateBack){
+            IconButton(onClick = navigateUp){
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDesciption=""
+                )
+            }
+        }
+    }
 
-){}
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
