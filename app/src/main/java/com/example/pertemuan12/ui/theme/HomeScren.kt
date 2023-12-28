0800e6f23866b8d68ad458e610e921bff539161d
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-12-28 09:54:20 +0700

    membuat sealedclass dan function homescren

---
+++ app/src/main/java/com/example/pertemuan12/ui/theme/HomeScren.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan12.ui.theme
+
+import com.example.pertemuan12.navigation.DestinasiNavigasi
+
+object DestinasiHome:DestinasiNavigasi{
+    override val route="home"
+    override val titleRes="Kontak"
+}
+
+
